c94f48f67e13709ae4368ad9b39f7df1e8f48d15
Shreya Bhattarai <sheriyaa321bhattaraii@gmail.com>
2023-08-14 18:31:13 +0545

    text

--- first.js
+++ first.js
@@ -1,17 +1,21 @@
 //prompt vaneko chai user input lina
+
 fruit = prompt('What is your favourite fruit?');
 console.log(fruit);
 alert(fruit);
 
 //Number use garnu ko reason chai hami sanga return aako value string ma hunxa
+
 food=Number(prompt('How much was the food?'));
 tipPercentage=Number(prompt('percent you want to tip based on food')/100);
 tipAmount= food*tipPercentage;
 console.log(tipAmount);
+
 //alert vaneko chai value dekhauna lai alert box ma 
 alert(tipAmount);
 
 //directly plus garda concatenate garxa
+
 totalAmount= food + tipAmount ;
 alert(totalAmount);
 
@@ -30,7 +34,3 @@ ceil-Math.ceil() method round the number to the nearest integer and returns the 
 random-Math.random() method returns a random number between 0(inclusive) and 1 (exclusive)
 Math.ranodm()*3 gives all random values between 0 and 3
 */
-
-
-
-
---
+++ weather.js
@@ -0,0 +1,18 @@
+/*let weather = 'rain';
+if (weather == 'rain'){
+    console.log('Grab your Umbrella ☔');
+}
+if(weather == 'sunny'){
+    console.log('Grab your Sunglasses 😎');
+}*/
+let weather = prompt('Whats the weather like?');
+if (true){
+    console.log('Grab your Umbrella ☔');
+}
+else {
+    console.log('Grab your Sunglasses 😎')
+}
+//conditional operators
+// ==,===,>,<,<=,>=,!=,!==
+//condtional are used in statements to comapre variables values and/or data types.
+//they always return boolean- true or false
