0793786235ff38069df0bdebdbcd0157296b79cf
Shreya Bhattarai <sheriyaa321bhattaraii@gmail.com>
2023-08-16 15:46:32 +0545

    txt

---
+++ practise.js
@@ -0,0 +1,41 @@
+/*const sumArray1 = (numberss) => {
+    let result=0;
+  for(const num of numberss){
+    result += num
+  }
+  return result;
+}
+const numb =[1,2,3]
+console.log(sumArray(numb))*/
+
+math= Math.floor(Math.random()*8)
+console.log(math)
+
+
+const hourToSecond =(hours) => {
+    console.log(hours)
+let second = (hours * 60) * 60
+return second
+}
+const userInput = prompt("Convert hour to second")
+console.log(hourToSecond(userInput))
+
+
+/*const letterCounter = (phrase) => {
+    return {
+        result: phrase.length
+    }
+}
+   const phrase = prompt("Write your phrase.")
+console.log(letterCounter(phrase))*/
+
+
+/*function calculateFood(food, tipPer){
+//food=food
+tipPercentage= tipPer/100;
+tipAmount= food * tipPercentage;
+
+total = sum(food, tipAmount ) ;
+return total;
+}
+console.log(calculateFood(100, 20));*/
