d903868b428b5511f434fd6382209419dfab0536
Shreya Bhattarai <sheriyaa321bhattaraii@gmail.com>
2023-08-28 19:51:26 +0545

    txt

--- API/superhero/script.js
+++ API/superhero/script.js
@@ -1,19 +1,4 @@
-/*const superHeroDiv=document.getElementById('superHero')
-//const heroo=document.getElementById('heroo')
-const imageDiv= document.getElementById("superHero")
 
-
-const SUPERHERO_TOKEN = '2516678318513173'
-const BASE_URL= 'https://superherospi.com/api.php/${SUPERHERO_TOKEN}'
-
-const generate = (id, name) => {
-   // const superHero = document.getElementById('superHero')
-    fetch(`${BASE_URL}/${id}`)
-    .then(response => response.json())
-    .then(json => document.querySelector('body').innerHTML += `<img src="${json.image.url}" height=200 width=200/>`)
-
-}
-generate(245)*/
 
 const superHeroDiv=document.getElementById('superHero')
 //const heroo=document.getElementById('heroo')
